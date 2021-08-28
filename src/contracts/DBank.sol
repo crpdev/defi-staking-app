c98d3d0abd846fc16ba174e01e8579ef3bdea482
Rajapandian C <rajapandianc@outlook.in>
2021-08-28 16:43:32 +0530

    Added staking logic for yield farming

--- src/contracts/DBank.sol
+++ src/contracts/DBank.sol
@@ -7,13 +7,61 @@ import './Reward.sol';
 
 contract DBank {
     address public owner;
-    string public name = 'Decental Bank';
+    string public name = 'Decentral Bank';
     Tether public tether;
     Reward public reward;
     
     constructor(Tether _tether, Reward _reward) {
+        owner = msg.sender;
         tether = _tether;
         reward = _reward;
     }
 
+    address[] public stakers;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, 'the caller must be the owner of this contract');
+        _;
+    }
+
+    // Mapping to keep track of account's staking
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
+    // Staking function to deposit USDT tokens
+    function depositTokens(uint256 _amount) public {
+
+        // Enforce condition to check if staking amount is greater than zero
+        require(_amount > 0, 'staking amount cannot be zero');
+
+        tether.transferFrom(msg.sender, address(this), _amount);
+        
+        // Update customer's staking balance
+        stakingBalance[msg.sender] += _amount;
+
+        if(!hasStaked[msg.sender]){
+            stakers.push(msg.sender);
+        }
+
+        hasStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
+    }
+
+    function issueRewardTokens() public onlyOwner {
+        for (uint i=0;i<stakers.length;i++){
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient] / 9;
+            require(balance > 0);
+            reward.transfer(recipient, balance);
+        }
+    }
+
+    function unstakeTokens() public {
+        uint256 balance = stakingBalance[msg.sender]; 
+        require(balance > 0, 'unstaking amount cannot be zero');
+        tether.transfer(msg.sender, balance);
+        stakingBalance[msg.sender] = 0;
+        isStaking[msg.sender] = false;
+    }
 }
--- test/dbank.tests.js
+++ test/dbank.tests.js
@@ -15,6 +15,8 @@ contract ('DBank', ([owner, customer]) => {
         tether = await Tether.new();
         reward = await Reward.new();
         dBank = await DBank.new(tether.address, reward.address);
+        console.log("DBank Deployed Address: ", await dBank.address);
+        console.log("DBank Owner address: ", await dBank.owner());
         await reward.transfer(dBank.address, tokens('1000000'));
         await tether.transfer(customer, tokens('100'), { from: owner});
     })
@@ -37,6 +39,30 @@ contract ('DBank', ([owner, customer]) => {
         it('contract has tokens', async() => {
             let balance = await reward.balanceOf(dBank.address);
             assert.equal(balance, tokens('1000000'));
+        });
+    });
+
+    describe('Staking Tokens', async () => {
+        it('rewards tokens for staking', async() => {
+            let result;
+            // Check customer balance
+            result = await tether.balanceOf(customer);
+            console.log("Customer balance: ", result.toString());
+            assert.equal(result.toString(), tokens('100'),'customer token balance before staking');
+            await tether.approve(dBank.address, tokens('100'), {from: customer});
+            await dBank.depositTokens(tokens('100'), {from: customer});
+            result = await tether.balanceOf(customer);
+            console.log("Customer balance: ", result.toString());
+            assert.equal(result.toString(), tokens('0'),'customer token balance after staking');
+            result = await tether.balanceOf(dBank.address);
+            console.log("Dbank balance: ", result.toString());
+            assert.equal(result.toString(), tokens('100'),'DBank token balance before staking');
+            result = await dBank.isStaking[customer];
+            assert.equal(result.toString(), 'true','Customer\'s staking status after staking');
+
+            await dBank.issueTokens({from: owner});
+
+            await dBank.issueTokens({from: customer}).should.be.rejected;
         });
     });
 });
